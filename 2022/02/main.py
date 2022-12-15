34cea3e90989e9c2fe61a32b350774974f813b9e
Chris Faux <cwuzhear@gmail.com>
2022-12-15 10:37:15 -0500

    2022-01 Part One

--- 2022/02/main.py
+++ 2022/02/main.py
@@ -1,3 +1,22 @@
 with open('input.txt') as file:
     lines = file.readlines()
 inputlist = [x[:-1] for x in lines]
+
+rps_results = {
+    'A X': 4,
+    'A Y': 8,
+    'A Z': 3,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 7,
+    'C Y': 2,
+    'C Z': 6
+}
+
+def part_one():
+    scores = [rps_results[x] for x in inputlist]
+    totalscore = sum(scores)
+    return totalscore
+
+print(part_one())
