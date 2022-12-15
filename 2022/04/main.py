d10bb5edbb7b59da1339b839487eb18656d22cde
Chris Faux <chrisfaux@gmail.com>
2022-12-15 13:36:38 -0500

    2022-04 Part One

--- 2022/04/main.py
+++ 2022/04/main.py
@@ -1,3 +1,12 @@
 with open('input.txt') as file:
     lines = file.readlines()
 inputlist = [x[:-1] for x in lines]
+
+
+def part_one():
+    pairslist = [x.split(',') for x in inputlist]
+    pairslist = [[i.split('-') for i in x] for x in pairslist]
+    pairslist = [[[int(i) for i in j] for j in x] for x in pairslist]
+    pairslist = [(x[0][0] <= x[1][0] and x[0][1] >= x[1][1]) or (
+        x[0][0] >= x[1][0] and x[0][1] <= x[1][1]) for x in pairslist]
+    print('Part One: ', sum(pairslist))
