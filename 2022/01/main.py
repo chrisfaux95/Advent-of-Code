3593b9cb5de7f47fa67cc63f5addbdd5b8c86436
Chris Faux <cwuzhear@gmail.com>
2022-12-15 10:01:33 -0500

    Completed 2022-01 Part One

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -1,3 +1,19 @@
 with open('input.txt') as file:
     lines = file.readlines()
-print(lines)
+l = [x[:-1] for x in lines]
+# print(l)
+
+def part_one(inp):
+    elves = []
+    currentelf = 0
+    for i in inp:
+        if i != '':
+            currentelf += int(i)
+        else:
+            elves.append(currentelf)
+            currentelf = 0
+    elves.append(currentelf)        
+    return (max(elves))
+
+print(part_one(l))
+
