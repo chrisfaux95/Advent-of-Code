ca485f36044042e53aad89cc7786f4450309f1d0
Chris Faux <cwuzhear@gmail.com>
2022-12-15 10:18:58 -0500

    cleaned up 2022-01

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -1,10 +1,9 @@
 with open('input.txt') as file:
     lines = file.readlines()
 inputlist = [x[:-1] for x in lines]
-# print(l)
 
 
-def part_one(inp):
+def getelves(inp):
     elves = []
     currentelf = 0
     for i in inp:
@@ -14,22 +13,19 @@ def part_one(inp):
             elves.append(currentelf)
             currentelf = 0
     elves.append(currentelf)
+    return (elves)
+
+
+def part_one():
+    elves = getelves(inputlist)
     return (max(elves))
 
 
-def part_two(inp):
-    elves = []
-    currentelf = 0
-    for i in inp:
-        if i != '':
-            currentelf += int(i)
-        else:
-            elves.append(currentelf)
-            currentelf = 0
-    elves.append(currentelf)
+def part_two():
+    elves = getelves(inputlist)
     sorted_elves = sorted(elves, reverse=True)
     return sum(sorted_elves[0:3])
 
 
-print(part_one(inputlist))
-print(part_two(inputlist))
+print(part_one())
+print(part_two())
