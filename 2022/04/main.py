b3e74830d5a4927fa0898d71cb7b27130c71a004
Chris Faux <chrisfaux@gmail.com>
2022-12-15 13:49:39 -0500

    2022-04 Part Two & Clean up

--- 2022/04/main.py
+++ 2022/04/main.py
@@ -1,12 +1,22 @@
 with open('input.txt') as file:
     lines = file.readlines()
 inputlist = [x[:-1] for x in lines]
+inputlist = [x.split(',') for x in inputlist]
+inputlist = [[i.split('-') for i in x] for x in inputlist]
+inputlist = [[[int(i) for i in j] for j in x] for x in inputlist]
 
 
 def part_one():
-    pairslist = [x.split(',') for x in inputlist]
-    pairslist = [[i.split('-') for i in x] for x in pairslist]
-    pairslist = [[[int(i) for i in j] for j in x] for x in pairslist]
     pairslist = [(x[0][0] <= x[1][0] and x[0][1] >= x[1][1]) or (
-        x[0][0] >= x[1][0] and x[0][1] <= x[1][1]) for x in pairslist]
+        x[0][0] >= x[1][0] and x[0][1] <= x[1][1]) for x in inputlist]
     print('Part One: ', sum(pairslist))
+
+
+def part_two():
+    pairslist = [(x[0][0] <= x[1][0] <= x[0][1] or x[1][0]
+                  <= x[0][0] <= x[1][1]) for x in inputlist]
+    print('Part Two: ', sum(pairslist))
+
+
+part_one()
+part_two()
